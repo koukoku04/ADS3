9b03247b6e31544d85e9b10f3a396bfa34973d37
batyr <batyrhan108@gmail.com>
2024-05-15 09:23:20 +0500

    implementation of testing field of hashtablefor 2nd part of project

---
+++ src/part_1_2/Main.java
@@ -0,0 +1,21 @@
+package part_1_2;
+
+import part_1_1.MyHashTable;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyHashTable<TestingField, String> table = new MyHashTable<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int id = rand.nextInt(1000);
+            TestingField object = new TestingField(id);
+            table.put(object, "Value " + i);
+        }
+
+        table.printBucketSizes();
+    }
+}
---
+++ src/part_1_2/TestingField.java
@@ -0,0 +1,35 @@
+package part_1_2;
+
+import java.util.Random;
+
+class TestingField {
+    private int id;
+
+    public TestingField(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        // Custom hash function for TestingField
+        int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TestingField that = (TestingField) obj;
+        return id == that.id;
+    }
+}
+
+
+
